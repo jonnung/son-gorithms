22a0e29290334b559f3bc3b66c95df36ee28f778
EunwooCho <jonnung@gmail.com>
2015-01-26 00:09:20 +0900

    동적 계획법 메모 수정

--- Lecture/2-1.DynamicPrograming/Listing7.py
+++ Lecture/2-1.DynamicPrograming/Listing7.py
@@ -1,1 +1,18 @@
 # -*- coding: utf8 -*-
+
+
+def recMC(coinValueList,change):
+    minCoins = change
+    if change in coinValueList:
+        return 1
+    else:
+        for i in [c for c in coinValueList if c <= change]:
+            numCoins = 1 + recMC(coinValueList,change-i)
+            if numCoins < minCoins:
+                minCoins = numCoins
+    return minCoins
+
+
+print(recMC([1, 5, 10, 25], 63))
+
+# 겁나게 느리군
