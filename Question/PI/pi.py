28372c599ee9649866a32466836c95eceefce63c
EunwooCho <jonnung@gmail.com>
2015-02-03 01:19:27 +0900

    동적계획법 심화, PI 문제 풀이, PI 다시 풀기(Doing)

--- Question/PI/pi.py
+++ Question/PI/pi.py
@@ -19,48 +19,42 @@ def is_level5(l):
     return all(x - y == d for x, y in zip(l, l[1:]))
 
 
+def calculate(pi):
+    # 리스트(pi)의 전체 길이(length)를 구함 (처음부터 끝까지 완전 탐색을 위한 것임)
+    pi_len = len(pi)
+    # 최적 부분 구조 리스트
+    optimize_sub = [0] * pi_len
+    # 현재 위치를 알아야 한다. (i)
+    i = 0
+    min_sum = 0
+    # 리스트 끝까지 갈때까지 무한루프(while) 시작
+    while i < pi_len:
+        # 현재 위치가 2보다 작으면 모두 레벨 10이다.(i가 1부터 시작하면 3보다 작을때가 해당)
+        if i < 2:
+            level = 10
+        else:
+            start = (i - 3) + 1
+            end = i + 1
+
+            # pi[(i - 3) + 1, i + 1]  # 1, 4 [1, 2, 3]
+            # pi[(i - 4) + 1, i + 1]  # 0, 4 [0, 1, 2, 3]
+            # pi[(i - 5) + 1, i + 1]  # -1, 4 [4, 0, 1, 2, 3]
+
+            if start == 0:
+                pass
+            elif start > 0:
+                pass
+            else:
+                pass
+            # 그 위치와 -3, -4, -5 전에 있던 것들과의 레벨을 각각 계산한다. (단, 이들 3개의 결과는 서로 영향을 주지 않는다.)
+            # 각각 계산된 레벨들과 그 범위들 바로 전 위치(예: i-3)의 계산된(저장된) 값의 합이 현재까지 알고 있는 가장 최소값보다 작으면 대체된다.
+            # 리스트의 끝까지 가기전에는 가장 최소값은 의미가 없다. 리스트 끝에서 계산되는 레벨합 중에 가장 최소값(최종해)을 구하면 된다.
+        optimize_sub[i] = level
+
+
+
 if __name__ == '__main__':
     tc = int(raw_input())
-    slice_list = [5, 4, 3]
-    cache5 = {}
-    cache4 = {}
-    cache3 = {}
 
-    for x in xrange(tc):
-        final_level_sum = 0
-        pi = [int(i) for i in raw_input()]
-        start = 0
-        end = 0
-        prev_list = [0, 0, 0]
-
-        while True:
-            for step in slice_list:
-                for i, v in enumerate(prev_list):
-                    prev_list[i] = v+step
-
-                    if (v+step+1) - v >= 3 or (v+step+1) - v == 0:
-                        current = tuple(pi[v:prev_list[i]])
-
-                        if step == 5:
-                            find_cache = cache5
-                        elif step == 4:
-                            find_cache = cache4
-                        elif step == 3:
-                            find_cache = cache3
-                        else:
-                            find_cache = []
-
-                        if current in find_cache:
-                            level = find_cache[current]
-                        elif is_level1(current):
-                            level = 1
-                        elif is_level2(current):
-                            level = 2
-                        elif is_level4(current):
-                            level = 4
-                        elif is_level5(current):
-                            level = 5
-                        else:
-                            level = 10
-                    else:
-                        pass
+    for c in xrange(tc):
+        print(calculate(tc))
